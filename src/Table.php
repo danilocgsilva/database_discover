5e0033f5bf128f2e053ff158a822411be0060c02
Danilo Silva <contact@danilocgsilva.me>
2023-10-24 13:23:17 +0000

    Allow get first field name

--- src/Table.php
+++ src/Table.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\Database;
 
+use PDO;
+
 class Table
 {
     private string $name;
     private ?int $size = NULL;
+    private ?string $firstField = NULL;
 
     public function setName(string $name): self
     {
@@ -34,5 +37,20 @@ class Table
     public function __toString()
     {
         return $this->getName();
+    }
+
+    public function fetchFirstField(PDO $pdo): self
+    {
+        $queryFields = sprintf("DESCRIBE %s;", $this->name);
+        $preResult = $pdo->prepare($queryFields);
+        $preResult->execute();
+        $firstOccurrence = $preResult->fetch(PDO::FETCH_NUM);
+        $this->firstField = firstOccurrence[0];
+        return $this;
+    }
+
+    public function getFirstField(): string
+    {
+        return $this->firstField;
     }
 }
