5ec41087bb4cc302fe60ed8306f33224b59317b7
Danilo Silva <contact@danilocgsilva.me>
2023-10-24 16:25:49 +0000

    Readded fetch first field

--- src/Table.php
+++ src/Table.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\Database;
 
+use PDO;
+
 class Table
 {
     private string $name;
     private ?int $size = NULL;
+    public readonly string $firstField;
 
     public function setName(string $name): self
     {
@@ -34,5 +37,16 @@ class Table
     public function __toString()
     {
         return $this->getName();
+    }
+
+    public function fetchFirstField(PDO $pdo): self
+    {
+        $query = sprintf("DESCRIBE %s;", $this->name);
+        $preResult = $pdo->prepare($query);
+        $preResult->execute();
+        $row = $preResult->fetch(PDO::FETCH_NUM);
+        $this->firstField = $row[0];
+        
+        return $this;
     }
 }
